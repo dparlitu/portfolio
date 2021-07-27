18ab2c27722a461781bb3546537ef54ecf09bcb2
dparlitu <DPARLITU@our.ecu.edu.au>
2021-07-27 11:46:51 +0000

    add comments to filenames.sh

--- week3/filenames.sh
+++ week3/filenames.sh
@@ -7,12 +7,16 @@ if [ -z "$1" ]; then
     echo "Error, provide the name of the file containing the search list" && exit 1 
 fi
 
+#if line is empty, just move on
 while read line; do
     if [ -z "$line" ]; then
         echo
     else
+        #check if is -d directory then echo
         if [ -d "$line" ]; then
             echo "$line is a Directory"
+        
+        #check if is -f file then echo
         elif [ -f "$line" ]; then
             echo "$line is a File"
         else
