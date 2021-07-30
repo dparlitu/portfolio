26722f123e41e6cab72aed405bdc7e4a6f266de6
dparlitu <DPARLITU@our.ecu.edu.au>
2021-07-30 13:25:37 +0000

    add colour and success messages

--- week3/megafoldermaker.sh
+++ week3/megafoldermaker.sh
@@ -1,23 +1,30 @@
 #!/bin/bash 
-      
-#If there aren't two arguments to the script 
+
+#If there aren't three arguments to the script 
       
 if (( $#!=3 )); then
 
-#Print an error and exit
+    #Print an error and exit
 
     echo "Error, provide three numbers" && exit 1 
       
 fi 
       
 #For every number between the first argument and the last 
-      
+
+echo
+
 for ((i = $1; i <= $3; i++)) 
       
-do
+    do
 
-    #Create a new folder for that number 
-    echo "Creating directory number $i" 
-    mkdir "week$i" 
-      
+        #Create a new folder for that number 
+        echo "Creating directory week$i"
+        mkdir "week$i"
+        echo -e "\033[32mweek$i created succesfully\e[0m"
+
 done
+
+echo
+
+exit 0
